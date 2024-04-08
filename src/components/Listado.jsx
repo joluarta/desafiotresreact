5b6f11e1a7d670cab03038f603d5d545939f285e
joluarta <joluarta78@gmail.com>
2024-04-07 23:09:45 -0400

    subiertrabajo

---
+++ src/components/Listado.jsx
@@ -0,0 +1,32 @@
+import Table from 'react-bootstrap/Table';
+
+const Listado = ({ colaboradores }) => {
+  return (
+    <>
+      <Table responsive striped bordered>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Edad</th>
+            <th>Cargo</th>
+            <th>Telefono</th>
+          </tr>
+        </thead>
+        <tbody>
+        {colaboradores.map((colaborador) => (
+            <tr key={colaborador.id}>
+              <td>{colaborador.nombre}</td>
+              <td>{colaborador.correo}</td>
+              <td>{colaborador.edad}</td>
+              <td>{colaborador.cargo}</td>
+              <td>{colaborador.telefono}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default Listado;
